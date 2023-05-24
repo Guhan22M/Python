35a55ffe8dc2534837fa3cb5ff4e47af55429294
Guhan22M <133367863+Guhan22M@users.noreply.github.com>
2023-05-24 10:25:07 +0530

    Delete check_triangle5.py

--- check_triangle5.py
+++
@@ -1,7 +0,0 @@
-a=int(input("Enter the first side length "))
-b=int(input("Enter the second side length "))
-c=int(input("enter the third side length "))
-if ((b+c)>a) and ((a+c)>b) and ((a+b)>c):
-    print("It is perfect triangle")
-else:
-    print("It is not a triangle")
