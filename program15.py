09dc4b2913b4161d34c0904a1889588bc5e11443
Guhan22M <133367863+Guhan22M@users.noreply.github.com>
2023-06-18 07:11:23 +0000

    factors

--- program15.py
+++ program15.py
@@ -1,8 +1,5 @@
 a=int(input("Enter the starting value "))
 b=int(input("Enter the ending value "))
 for i in range(a,b):
-    if i%2==0 or i%3==0:
-        
-    
-    
-        
+    if b%i==0:
+        print(i)
